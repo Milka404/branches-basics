7982d062d6a902cf23252aa728f0c02aa8dc2163
Milka404 <Milka2158@gmail.com>
2018-02-20 19:10:41 +0700

    change

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "iostream"
+#include "math.h"
 using namespace std;
 
 int main()
 {
-	int b, x, i;
+	int b=5, x, i;
 	 x=0;
 	 for (i=0; i<15; i++)
 	 {
-	 	x+=i;
+	   x+=i;
 	cout << x <<"\n";
 
 	 }
-	 //cout << i<<"\n";
+	for ( int j=0; j<10; j++)
+	{
+	   b+=j;
+	cout << b<< "\n";
+	}
+
+	cout <<x+b<<endl<< "\n";
 	return 0;
 }
