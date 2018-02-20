a13e744264b78c6ea0450670d9a012495252742c
Milka404 <Milka2158@gmail.com>
2018-02-20 18:03:21 +0700

    1 primer

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	int b, x, i;
+	 x=0;
+	 for (i=0; i<15; i++)
+	 {
+	 	x+=i;
+	cout << x <<"\n";
+
+	 }
+	 //cout << i<<"\n";
+	return 0;
+}
